2eba561276df28dc57f481be093c31555a0fb27d
omerpaz95 <omerpaz95@gmail.com>
2026-03-16 12:02:36 +0200

    Fixed a small bug in async_scheduler.

--- vllm/v1/core/sched/async_scheduler.py
+++ vllm/v1/core/sched/async_scheduler.py
@@ -19,7 +19,9 @@ class AsyncScheduler(Scheduler):
         super()._update_after_schedule(scheduler_output)
         spec_decode_tokens = scheduler_output.scheduled_spec_decode_tokens
         for req_id in scheduler_output.num_scheduled_tokens:
-            request = self.requests[req_id]
+            request = self.requests.get(req_id)
+            if request is None:
+                continue
             if request.is_prefill_chunk:
                 continue
 
