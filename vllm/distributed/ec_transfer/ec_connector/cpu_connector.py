7b4d86b993b2f983b4fb99c39a07ba11f78a0656
omerpaz95 <omerpaz95@gmail.com>
2026-04-30 16:55:04 +0300

    Fixed some code review comments and moved import.
    Signed-off-by: omerpaz95 <omerpaz95@gmail.com>

--- vllm/distributed/ec_transfer/ec_connector/cpu_connector.py
+++ vllm/distributed/ec_transfer/ec_connector/cpu_connector.py
@@ -12,6 +12,7 @@ from vllm.distributed.ec_transfer.ec_connector.base import (
     ECConnectorRole,
 )
 from vllm.logger import init_logger
+from vllm.platforms import current_platform
 from vllm.v1.core.sched.output import SchedulerOutput
 
 if TYPE_CHECKING:
@@ -37,23 +38,30 @@ class ECCPUConnector(ECConnectorBase):
         # mm_hash -> (offset, size) in the flat CPU buffer
         self._encoding_map: dict[str, tuple[int, int]] = {}
 
+        self._cpu_buffer: torch.Tensor | None = None
+
     # ==============================
     # Worker-side methods
     # ==============================
 
     def register_caches(self, ec_caches: dict[str, torch.Tensor]):
+        """
+        This needs to be called by someone
+        Or, alternatively, create the cpu buffer in init().
+        """
+
+        if "cpu_buffer" not in ec_caches:
+            raise ValueError("ECCPUConnector requires 'cpu_buffer' in ec_caches")
         self._cpu_buffer = ec_caches["cpu_buffer"]
 
     def start_load_caches(
         self, encoder_cache: dict[str, torch.Tensor], **kwargs
     ) -> None:
-        from vllm.platforms import current_platform
-
         metadata = self._get_connector_metadata()
         assert isinstance(metadata, ECCPUConnectorMetadata)
 
         for mm_hash, (offset, size) in metadata.mm_hash_to_cpu_loc.items():
-            if mm_hash in encoder_cache:
+            if mm_hash in encoder_cache or not self._cpu_buffer:
                 continue
             encoder_cache[mm_hash] = self._cpu_buffer[offset : offset + size].to(
                 device=current_platform.device_type, non_blocking=True
